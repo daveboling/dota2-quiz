0eb69109f76a14019c421d340d692ec35d9d1bf6
Dave Boling <daviddboling@gmail.com>
2014-03-05 02:58:06 -0500

    3/5/14 - Final functional revision.
    - Final revisions to overall functionality have officially been made.
    - All that's left is to draft all the questions!
    - Thanks for all the fish.

--- js/basic.js
+++ js/basic.js
@@ -2,7 +2,6 @@ var basic = [
 //Question 1
 	{
 		pic: false,
-		img: undefined,
 		question: "What is the respawn time on Roshan after he has been slain?",
 		choices: ["Between 8-11 minutes", "8 Minutes", "11 Minutes", "None of these"],
 		answer: 0,
@@ -24,6 +23,29 @@ var basic = [
 		question: "Which character is this?",
 		choices: ["Sven", "Ancient Apparition", "Visage", "Lich"],
 		answer: 1,
+	},
+//Question 4
+	{
+		pic: true,
+		img: '<img src="images/icons/agi.png" alt="" />',
+		src: '../images/icons/agi.png',
+		question: "What two basic things does Agility grant?",
+		choices: ["Armor and Health", "Damage and Attack Speed", "Armor and Damage", "Armor and Attack Speed"],
+		answer: 3,
+	},
+//Question 5
+	{
+		pic: false,
+		question: "For every point of agility a hero gains, how much attack speed and armor do they gain?",
+		choices: ["0.15 Armor, 2 Atk Speed", "0.14 Armor, 1 Atk Speed", "0.15 Armor, 1 Atk Speed", "0.14 Armor, 2 Atk Speed"],
+		answer: 1,
+	},
+//Question 5
+	{
+		pic: false,
+		question: "What extra attribute does an Agility character get when getting agility?",
+		choices: ["Armor","Attack Speed", "Damage", "Mana Regen", "Health Regen"],
+		answer: 2,
 	},
 //Question EMPTY
 	{
--- js/mainApp.js
+++ js/mainApp.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+//Score bar!
+var level;
+function progress(percent, $element) {
+	level = 1;
+	var progressBarWidth = percent * $element.width() / 100;
+
+	for (var i = 1; i <= 25; i++){
+		if(percent > ((i / 25) * 100)){level++;$('.level').text(level);}
+	}
+
+	$element.find('div').animate({ width: progressBarWidth }, 500);
+}
+
 
 /* PRE LOADER */
 //All other images
@@ -10,9 +23,8 @@ var misc_images = [
 
 var loadPercent = 0;
 var images = [];
-var k;
 function preload(array) {
-	for (k = 0; k < array.length; k++) {
+	for (var k = 0; k < array.length; k++) {
 		if (array[k].pic === true)
 		{
 			images[k] = new Image();
@@ -21,17 +33,18 @@ function preload(array) {
 		}
 		
 	}
+	loadPercent = (k / array.length) * 100;
 
 }
-//What to load? Put it here!
+
+//What do we need to preload? Put it all here!
 preload(misc_images);
 preload(basic);
 
-//Percentage bar
-loadPercent = k / images.length * 75;
+//Send % complete to percentage bar
 $('#perc').html(loadPercent);
 
-//Load intro if done loading
+//If done loading, show the 'ready' image.
 if (loadPercent === 100){
 	$('#loading').fadeOut(1000, function () {
 		$('#three').fadeIn(500, function () {
@@ -42,7 +55,7 @@ if (loadPercent === 100){
 	});
 }
 
-//If DOTA 2 Symbol is clicked 
+
 $('#loading_screen img').on('click', function(){
 	$('#loading_screen').fadeOut(1000, function () {
 		$('#base_menu').fadeIn(1000);
@@ -50,46 +63,39 @@ $('#loading_screen img').on('click', function(){
 	
 });
 
-
-
-/* PRE LOADER END 
-------------------
-------------------*/
-
-/* ARROW ANIMATION */
-var arr = $('.arrow');
-function arrow() {
-arr.animate({left: '+=5%'}, 300, function () {
-	arr.animate({left: '-=5%'}, 300, function(){
-		arrow();
+$('#game_over').find('.again').on('click', function(){
+	progress(0, $('#xp_bar'));
+	$('#game_over').fadeOut(1000, function () {
+		$('#base_menu').fadeIn(1000);
+		level = 1;
+		$('.level').html(level);
 	});
 
 });
 
+
+
+/* ARROW ANIMATION */
+function arrow() {
+var arr = $('.arrow');
+	arr.animate({left: '+=5%'}, 300, function () {
+		arr.animate({left: '-=5%'}, 300, function(){
+			arrow();
+		});
+	});
 }
 
 arrow();
 
 
 
-
 /* BASE MENU SELECTION */
-//Hide the .base_menu on choice
 $('#basic').on('click', function () {
-
-	$('#base_menu').hide();
-
-	//Load basic quiz
-	quiz(basic);
-
-	$('#quiz').fadeIn(500);
+	$('#base_menu').fadeOut(1000, function () {
+			quiz(basic);
+			$('#quiz').fadeIn(500);
+	});
 });
-/* BASE MENU SELECTION END
---------------------------
---------------------------*/
-
-
-
 
 
 
@@ -100,48 +106,62 @@ var answer;
 
 function quiz (array) {
 	var i = 0;
-	var score = 0;
-	
-
+	var score = 1;
 
 	loadQuestion(); //initial load
 
-	$('.q_button').on('click', checkAnswer);
-
+	$('.q_button').on('click', checkAnswer); //Turn on the event
 
 function checkAnswer() {
+	var levelSpan = $('.level').text();
 
-		$('.q_button').off('click', checkAnswer);
+		$('.q_button').off('click', checkAnswer); //Turn it off during checks
 
 		if (this.innerHTML == array[i].choices[answer]){
-
 			$('#correct').html('<div class="check"></div>Correct ').fadeIn(2000);
 			score++;
+			progress((score / array.length  * 100), $('#xp_bar'));
 		}
 		else {
-			$('#correct').html('<div class="x"></div> Correct Answer: ' + array[i].choices[answer]).fadeIn(2000);
+			$('#correct').html('<div class="x"></div>Correct Answer: ' + array[i].choices[answer]).fadeIn(2000);
 		}
 
-
+		//Give user enough time to see answer
 		setTimeout(function () {
 			i++;
 			$('.q_button').on('click', checkAnswer);
 			loadQuestion();
 		}, 2000);
 
+		//Game Over Menu
 		if(i === array.length - 2){
 		setTimeout(function () {
 			loadQuestion();
-
 			q_menu.hide();
+
 			game_over.fadeIn();
-			game_over.find('#score').text(score);
+			if(level >= 1 && level <= 5){game_over.find('.message').html("Intentional feeder. <br />You have no idea what you're doing yet.<br /> That's ok, keep going!");}
+			if(level >= 6 && level <= 10){game_over.find('.message').html("Game is hard. <br />So no more feeding?<br />Let's see some hustle.");}
+			if(level >= 11 && level <= 15){game_over.find('.message').html("Getting there.<br />You can at least survive and have a positive score. <br />Still lots of work to be done.");}
+			if(level >= 14 && level <= 20){game_over.find('.message').html("You're competent.<br />You can hold your own in most situations.");}
+			if(level >= 21 && level <= 25){game_over.find('.message').html("You are a Sensei.<br />Your only limitations are not being psychic. <br/> What are you waiting for? <br />Carry your team to victory! ");}
+
+			$('#score').html(level);
+
+
+			//RESETS
+			i = -1;
+			score = 0;
+
+
 		}, 2000);
-		}
+		}//End Game Over
 
 	
 
 }
+
+
 
 
 function loadQuestion(){
@@ -152,6 +172,7 @@ function loadQuestion(){
 	if(array[i].pic === true){
 		q_menu.find('.img_holder').html(array[i].img);
 	}
+	else { q_menu.find('.img_holder').html(''); }
 
 	//Load question
 	q_menu.find('.question').text(array[i].question);
